2e06fcea823b052a4916a42d2c40e02a8c023a79
Mafinar K <mafinar@gmail.com>
2024-10-28 11:37:59 -0400

    Add Library.fs 2015

---
+++ Year2015/Library.fs
@@ -0,0 +1,9 @@
+﻿namespace Year2015
+
+open Common.Types
+
+module Solver =
+    let solveFor input day =
+        match day with
+        | 1 -> Day01.solve input |> Ok
+        | _ -> NotDoneYet |> Error
