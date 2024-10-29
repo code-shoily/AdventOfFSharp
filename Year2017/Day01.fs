4f2e638dca3b5bddbaf20abe80e3aa8b7c019363
Mafinar K <mafinar@gmail.com>
2024-10-29 01:25:42 -0400

    Solve 2017/1

--- Year2017/Day01.fs
+++ Year2017/Day01.fs
@@ -1,10 +1,27 @@
-/// Year 2017/1 - ???
+/// Year 2017/1 - Inverse Captcha
 /// Link: https://adventofcode.com/2017/day/1
-/// Difficulty: 
-/// Tags: 
-/// Remarks:
+/// Difficulty: xs
+/// Tags: array
+/// Remarks: -
 module Year2017.Day01
-           
+
 open Common.Types
-           
-let solve (_: string seq) = Todo
+
+let solvePart1 =
+    (fun x -> Array.append x [| Array.head x |])
+    >> Array.windowed 2
+    >> Array.filter (fun arr -> arr[0] = arr[1])
+    >> Array.map Array.head
+    >> Array.sum
+
+let solvePart2 =
+    Array.splitInto 2
+    >> (fun pair -> Seq.zip pair[0] pair[1])
+    >> Seq.map (function
+        | a, b when a = b -> a + b
+        | _ -> 0)
+    >> Seq.sum
+
+let solve (rawInput: string seq) =
+    let input = rawInput |> Seq.head |> Seq.map (string >> int) |> Array.ofSeq
+    BothInt(solvePart1 input, solvePart2 input)
