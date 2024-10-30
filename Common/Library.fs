b7c47197b398b9bb1a77e73d84bd50607f080c7f
Mafinar K <mafinar@gmail.com>
2024-10-30 00:20:33 -0400

    Add paragraphs helper function

--- Common/Library.fs
+++ Common/Library.fs
@@ -3,6 +3,19 @@
 module Helpers =
     let unreachable () = failwith "unreachable"
     let todo () = failwith "todo"
+    
+    /// <summary>
+    /// Collects paragraphs (i.e. groups data separated by "" or "\n\n" in String form)
+    /// </summary>
+    /// <param name="input">Sequence of String as received from file</param>
+    let paragraphs input =
+        let rec construct paragraph result =
+            function
+            | [] -> List.rev (paragraph :: result)
+            | "" :: rest -> construct [] (paragraph :: result) rest
+            | head :: rest -> construct (head :: paragraph) result rest
+
+        construct [] [] input
 
 module Types =
     type Solution =
