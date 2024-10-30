4b83cc6d825347b7ed68b61ea7d8c375c9c28df1
Mafinar K <mafinar@gmail.com>
2024-10-30 00:20:43 -0400

    Solve 2022/1

--- Solutions.Tests/Year2022Tests.fs
+++ Solutions.Tests/Year2022Tests.fs
@@ -8,11 +8,11 @@ module TestYear2022Solutions =
 
     let getInput = Helpers.getInput 2022
 
-    (* [<Fact>]
+    [<Fact>]
     let ``Solves Day 1`` () =
         let day = 1
-        let expected = BothInt(232, 1783)
+        let expected = BothInt(70720, 207148)
 
-        let actual = Year2015.Day01.solve (getInput day)
+        let actual = Year2022.Day01.solve (getInput day)
 
-        Assert.Equal(expected, actual) *)
+        Assert.Equal(expected, actual)
--- Year2022/Day01.fs
+++ Year2022/Day01.fs
@@ -1,10 +1,22 @@
-/// Year 2022/1 - ???
+/// Year 2022/1 - Calorie Counting
 /// Link: https://adventofcode.com/2022/day/1
-/// Difficulty: 
-/// Tags: 
+/// Difficulty: xs
+/// Tags: sort
 /// Remarks:
 module Year2022.Day01
-           
+
+open Common.Helpers
 open Common.Types
-           
-let solve (_: string seq) = Todo
+
+let parse =
+    Seq.toList
+    >> paragraphs
+    >> List.map (List.map int >> List.sum)
+    >> List.sortDescending
+
+let solvePart1 = List.head
+let solvePart2 = List.take (3) >> List.sum
+
+let solve (rawInput: string seq) =
+    let input = parse rawInput
+    BothInt(solvePart1 input, solvePart2 input)
