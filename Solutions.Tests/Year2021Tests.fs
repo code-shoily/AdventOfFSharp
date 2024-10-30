31d2d082cb98c26dfc0fa9365ca32b32cf09f49b
Mafinar K <mafinar@gmail.com>
2024-10-29 22:55:47 -0400

    Solve 2021/1

--- Solutions.Tests/Year2021Tests.fs
+++ Solutions.Tests/Year2021Tests.fs
@@ -8,11 +8,11 @@ module TestYear2021Solutions =
 
     let getInput = Helpers.getInput 2021
 
-    (* [<Fact>]
+    [<Fact>]
     let ``Solves Day 1`` () =
         let day = 1
-        let expected = BothInt(232, 1783)
+        let expected = BothInt(1139, 1103)
 
-        let actual = Year2015.Day01.solve (getInput day)
+        let actual = Year2021.Day01.solve (getInput day)
 
-        Assert.Equal(expected, actual) *)
+        Assert.Equal(expected, actual)
--- Year2021/Day01.fs
+++ Year2021/Day01.fs
@@ -1,10 +1,21 @@
-/// Year 2021/1 - ???
+/// Year 2021/1 - Sonar Sweep
 /// Link: https://adventofcode.com/2021/day/1
-/// Difficulty: 
-/// Tags: 
+/// Difficulty: xs
+/// Tags: sliding-window
 /// Remarks:
 module Year2021.Day01
-           
+
 open Common.Types
-           
-let solve (_: string seq) = Todo
+
+let measurementWindow size =
+    Seq.windowed size
+    >> Seq.filter (fun arr -> (Array.head arr) < (Array.last arr))
+    >> Seq.length
+
+let solvePart1 = measurementWindow 2
+let solvePart2 = measurementWindow 4
+
+let solve (rawInput: string seq) =
+    let input = rawInput |> Seq.map (string >> int)
+
+    BothInt(solvePart1 input, solvePart2 input)
