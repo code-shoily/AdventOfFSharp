a6049757bdf3e2f9a6063e0acc9a9e7a8dc77c0e
Mafinar K <mafinar@gmail.com>
2024-11-02 23:08:52 -0400

    Add worksheet

---
+++ worksheet.fsx
@@ -0,0 +1,14 @@
+﻿#r "C:/Users/mafin/repos/fsharp/AdventOfFSharp/IOUtils/bin/Debug/net8.0/IOUtils.dll"
+#r "C:/Users/mafin/repos/fsharp/AdventOfFSharp/Common/bin/Debug/net8.0/Common.dll"
+
+open System
+open AdventOfCode.FSharp.IOUtils
+
+let getInput year day = readLines year day |> Option.get
+
+let rawInput = getInput 2016 2
+
+(* Copy Pasta *)
+
+
+(* Experiments go here *)
