234254b68feb727cf3332d7e3a241da92d831181
Mafinar K <mafinar@gmail.com>
2024-11-14 23:03:31 -0500

    Refactor 2019/3

--- Year2019/Day03.fs
+++ Year2019/Day03.fs
@@ -1,78 +1,150 @@
 /// Year 2019/3 - Crossed Wires
 /// Link: https://adventofcode.com/2019/day/3
-/// Difficulty: s
-/// Tags: navigation2d set slow
-/// Remarks: Refactor, use geometry
+/// Difficulty: m
+/// Tags: set geometry
+/// Remarks:
 module Year2019.Day03
 
 open Common.Helpers
 open Common.Types
 
-module Wire =
+[<AutoOpen>]
+module Geometry =
     type Direction =
-        | Left of int
-        | Right of int
         | Up of int
         | Down of int
+        | Left of int
+        | Right of int
 
-    let step (x, y) =
-        (function
-        | Up steps ->
-            [ for i in [ 1..steps ] do
-                  (x, y + i) ]
-        | Down steps ->
-            [ for i in [ 1..steps ] do
-                  (x, y - i) ]
-        | Left steps ->
-            [ for i in [ 1..steps ] do
-                  (x - i, y) ]
-        | Right steps ->
-            [ for i in [ 1..steps ] do
-                  (x + i, y) ])
+        member this.Steps =
+            match this with
+            | Up x -> x
+            | Down x -> x
+            | Left x -> x
+            | Right x -> x
 
-    let draw (directions: Direction seq) =
-        let wirePath =
-            ([ (0, 0) ], directions)
-            ||> Seq.fold (fun path -> step (Seq.last path) >> List.append path)
+    let drawLineFrom (x, y) =
+        function
+        | Up steps as up -> (x, y + 1), (x, y + steps), up
+        | Down steps as down -> (x, y - 1), (x, y - steps), down
+        | Left steps as left -> (x - 1, y), (x - steps, y), left
+        | Right steps as right -> (x + 1, y), (x + steps, y), right
 
-        ((0, Map.empty), wirePath)
-        ||> Seq.fold (fun (idx, visits) point ->
-            let updatedVisits =
-                match visits |> Map.containsKey point with
-                | true -> visits
-                | false -> visits |> Map.add point idx
+    let areIntersectingLines: int * int -> (int * int) * (int * int) -> bool =
+        fun (x, y) ->
+            function
+            | (xi, yi), (xf, yf) when xi = xf -> y >= min yi yf && y <= max yi yf
+            | (xi, yi), (xf, yf) when yi = yf -> x >= min xi xf && x <= max xi xf
+            | _ -> false
 
-            (idx + 1, updatedVisits))
-        |> snd
-        |> Map.remove (0, 0)
+    let positionInLine (x, y) =
+        function
+        | (xi, yi), (xf, yf) when xi = xf ->
+            if x = xi && (y >= min yi yf && y <= max yi yf) then
+                Some(abs (y - yi))
+            else
+                None
+        | (xi, yi), (xf, yf) when yi = yf ->
+            if y = yi && (x >= min xi xf && x <= max xi xf) then
+                Some(abs (x - xi))
+            else
+                None
+        | _ -> None
 
-    let parse (input: string seq) =
-        let translate (wireRep: string) : Direction =
-            match wireRep[0], wireRep[1..] with
-            | 'R', steps -> Right(int steps)
-            | 'L', steps -> Left(int steps)
-            | 'U', steps -> Up(int steps)
-            | 'D', steps -> Down(int steps)
+    let rec intersection
+        : ((int * int) * (int * int) * Direction) * ((int * int) * (int * int) * Direction) -> (int * int) option =
+        function
+        | ((xvi, yvi), (xvf, yvf), vertical), ((xhi, yhi), (xhf, yhf), horizontal) when
+            (vertical.IsUp || vertical.IsDown) || (horizontal.IsLeft || horizontal.IsRight)
+            ->
+            let isOnLeft = ((xvi, yvi), (xvf, yvf)) |> areIntersectingLines (xvi, yhf)
+            let isOnRight = ((xhi, yhi), (xhf, yhf)) |> areIntersectingLines (xvi, yhf)
+            if isOnLeft && isOnRight then Some((xvi, yhf)) else None
+
+        | horizontal, vertical -> intersection (vertical, horizontal)
+
+    let manhattan (x, y) = abs x + abs y
+
+module InputParser =
+    type WireConnections =
+        { LeftSegments: ((int * int) * (int * int) * Direction) list
+          RightSegments: ((int * int) * (int * int) * Direction) list }
+
+        member this.intersectionOfBothWires =
+            let partitionByOrientationVH (segment: ('a * 'a * Direction) list) =
+                segment |> List.partition (fun (_, _, dir) -> dir.IsUp || dir.IsDown)
+
+            let leftVertical, leftHorizontal = partitionByOrientationVH this.LeftSegments
+            let rightVertical, rightHorizontal = partitionByOrientationVH this.RightSegments
+
+            let findAllIntersections pair =
+                pair ||> List.allPairs |> List.choose intersection
+
+            [ (leftVertical, rightHorizontal) |> findAllIntersections
+              (leftHorizontal, rightVertical) |> findAllIntersections ]
+            |> List.concat
+
+    let getWireDirections: string -> Direction[] =
+        let getDirection (rep: string) =
+            match (rep[0], rep[1..]) with
+            | 'U', step -> Up(int step)
+            | 'D', step -> Down(int step)
+            | 'L', step -> Left(int step)
+            | 'R', step -> Right(int step)
             | _ -> unreachable ()
 
-        let left, right =
-            input
-            |> Seq.map (_.Split(",") >> Seq.map translate)
-            |> (fun x -> Seq.head x, Seq.last x)
+        _.Split(",") >> Array.map getDirection
 
-        draw left, draw right
+    let getWires: string seq -> Direction[] * Direction[] =
+        List.ofSeq
+        >> List.map getWireDirections
+        >> function
+            | [ a; b ] -> (a, b)
+            | _ -> unreachable ()
 
-let solvePart1 = Seq.map (fun (a, b) -> abs a + abs b) >> Seq.min
+    let createWireConnections leftDirs rightDirs =
+        let getSegments (dirs: Direction[]) =
+            ({| Point = (0, 0); Segments = [] |}, dirs)
+            ||> Seq.fold (fun state direction ->
+                let _, endPoint, _ as data = drawLineFrom state.Point direction
 
-let solvePart2 (leftPath: Map<int * int, int>) (rightPath: Map<int * int, int>) =
-    Seq.map (fun point -> leftPath[point] + rightPath[point]) >> Seq.min
+                {| state with
+                    Point = endPoint
+                    Segments = data :: state.Segments |})
+            |> _.Segments
+            |> List.rev
+
+        { LeftSegments = getSegments leftDirs
+          RightSegments = getSegments rightDirs }
+
+    let parse (rawInput: string seq) =
+        rawInput |> getWires ||> createWireConnections
+
+
+let solvePart1 = List.map manhattan >> List.min
+
+let solvePart2 (wires: InputParser.WireConnections) (common: (int * int) list) =
+    let findLeastSteps (wireSegments: ((int * int) * (int * int) * Direction) seq) =
+        common
+        |> Seq.map (fun (x, y) ->
+            ({| Found = None; Steps = 1 |}, wireSegments)
+            ||> Seq.scan (fun state (lineFrom, lineTo, direction) ->
+                match (lineFrom, lineTo) |> positionInLine (x, y) with
+                | Some(index) ->
+                    {| state with
+                        Found = Some(index + state.Steps) |}
+                | None ->
+                    {| state with
+                        Steps = direction.Steps + state.Steps |})
+            |> Seq.pick _.Found)
+        |> List.ofSeq
+
+    (findLeastSteps wires.LeftSegments, findLeastSteps wires.RightSegments)
+    ||> List.zip
+    |> List.minBy (fun pair -> pair ||> (+))
+    ||> (+)
 
 let solve (rawInput: string seq) =
-    let wireLeft, wireRight = Wire.parse rawInput
-
-    let intersections =
-        (wireLeft, wireRight)
-        ||> (fun a b -> Set.ofSeq a.Keys, Set.ofSeq b.Keys)
-        ||> Set.intersect
-
-    BothInt(intersections |> solvePart1, intersections |> solvePart2 wireLeft wireRight)
+    let wires = InputParser.parse rawInput
+    let common = wires.intersectionOfBothWires
+    BothInt(solvePart1 common, solvePart2 wires common)
--- worksheet.fsx
+++ worksheet.fsx
@@ -1,6 +1,7 @@
 ﻿#r "C:/Users/mafin/repos/fsharp/AdventOfFSharp/IOUtils/bin/Debug/net9.0/IOUtils.dll"
 #r "C:/Users/mafin/repos/fsharp/AdventOfFSharp/Common/bin/Debug/net9.0/Common.dll"
 
+open System
 open AdventOfCode.FSharp.IOUtils
 
 let getInput year day = readLines year day |> Option.get
@@ -8,106 +9,5 @@ let getInput year day = readLines year day |> Option.get
 let rawInput = getInput 2019 3
 
 (* Copy Pasta *)
-open Common.Helpers
-
-[<AutoOpen>]
-module Types =
-    type Direction =
-        | Up of int
-        | Down of int
-        | Left of int
-        | Right of int
-
-    type Orientation =
-        | Vertical of int
-        | Horizontal of int
-
-    type WireConnections =
-        { LeftSegments: ((int * int) * (int * int) * Orientation) list
-          RightSegments: ((int * int) * (int * int) * Orientation) list }
-
-    let drawLineFrom (x, y) =
-        function
-        | Up steps -> (x, y + 1), (x, y + steps), Vertical steps
-        | Down steps -> (x, y - 1), (x, y - steps), Vertical steps
-        | Left steps -> (x - 1, y), (x - steps, y), Horizontal steps
-        | Right steps -> (x + 1, y), (x + steps, y), Horizontal steps
-
-    let getWireDirections: string -> Direction[] =
-        let getDirection (rep: string) =
-            match (rep[0], rep[1..]) with
-            | 'U', step -> Up(int step)
-            | 'D', step -> Down(int step)
-            | 'L', step -> Left(int step)
-            | 'R', step -> Right(int step)
-            | _ -> unreachable ()
-
-        _.Split(",") >> Array.map getDirection
-
-    let getWires: string seq -> Direction[] * Direction[] =
-        List.ofSeq
-        >> List.map getWireDirections
-        >> function
-            | [ a; b ] -> (a, b)
-            | _ -> unreachable ()
-
-[<AutoOpen>]
-module Geometry =
-    let containedIn (x, y) =
-        function
-        | (xi, yi), (xf, yf) when xi = xf -> y >= min yi yf && y <= max yi yf
-        | (xi, yi), (xf, yf) when yi = yf -> x >= min xi xf && x <= max xi xf
-        | _ -> false
-
-    let rec intersection =
-        function
-        | ((xvi, yvi), (xvf, yvf), Vertical _), ((xhi, yhi), (xhf, yhf), Horizontal _) ->
-            let isOnLeft = ((xvi, yvi), (xvf, yvf)) |> containedIn (xvi, yhf)
-            let isOnRight = ((xhi, yhi), (xhf, yhf)) |> containedIn (xvi, yhf)
-            if isOnLeft && isOnRight then Some((xvi, yhf)) else None
-
-        | horizontal, vertical -> intersection (vertical, horizontal)
-
-    let manhattan (x, y) = abs x + abs y
-
-
-let intersectionOfBothWires left right =
-    let partitionByOrientationVH =
-        List.partition (function
-            | _, _, Vertical _ -> true
-            | _ -> false)
-
-    let leftVertical, leftHorizontal = partitionByOrientationVH left
-    let rightVertical, rightHorizontal = partitionByOrientationVH right
-
-    let findAllIntersections pair =
-        pair ||> List.allPairs |> List.choose intersection
-
-    [ (leftVertical, rightHorizontal) |> findAllIntersections
-      (leftHorizontal, rightVertical) |> findAllIntersections ]
-    |> List.concat
-
-let parse (rawInput: string seq) =
-    let left, right = rawInput |> getWires
-
-    let getSegments (dirs: Direction[]) =
-        (((0, 0), []), dirs)
-        ||> Seq.fold (fun (fromPoint, segments) direction ->
-            let _, endPoint, _ as data = drawLineFrom fromPoint direction
-            (endPoint, data :: segments))
-        |> snd
-        |> List.rev
-
-    { LeftSegments = getSegments left
-      RightSegments = getSegments right }
-
-
-let solvePart1 (wires: WireConnections) =
-    (wires.LeftSegments, wires.RightSegments)
-    ||> intersectionOfBothWires
-    |> List.map manhattan
-    |> List.min
 
 (* Experiments go here *)
-let data = rawInput |> parse
-solvePart1 data
