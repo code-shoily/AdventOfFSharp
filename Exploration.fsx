df39cd853ec8fee8bfc23154618be1f02c792283
Mafinar K <mafinar@gmail.com>
2025-01-31 12:35:08 -0500

    Add helper to collect stats from source code.

--- Exploration.fsx
+++ Exploration.fsx
@@ -3,9 +3,16 @@
 
 open IOUtils
 
-let captureInputAs f year month =
-    readLines year month
-    |> (Option.map f)
+let captureInputAs f year month = readLines year month |> (Option.map f)
 
 let inputAsString = captureInputAs (String.concat "\n")
 let inputAsList = captureInputAs List.ofSeq
+
+#r "./Meta/bin/Debug/net9.0/Meta.dll"
+
+open Meta
+open Meta.Solution
+
+let basePath = __SOURCE_DIRECTORY__
+
+basePath |> Stats.FromSrcFile 2020 1
