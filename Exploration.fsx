87cb9150ef0e74bc3de7651da0cc654cbbe63eda
Mafinar K <mafinar@gmail.com>
2025-02-01 00:04:49 -0500

    Add Meta module

--- Exploration.fsx
+++ Exploration.fsx
@@ -1,6 +1,7 @@
 ﻿#r "./IOUtils/bin/Debug/net9.0/IOUtils.dll"
 #r "./Common/bin/Debug/net9.0/Common.dll"
 
+open System.IO
 open IOUtils
 
 let captureInputAs f year month = readLines year month |> (Option.map f)
@@ -9,10 +10,3 @@ let inputAsString = captureInputAs (String.concat "\n")
 let inputAsList = captureInputAs List.ofSeq
 
 #r "./Meta/bin/Debug/net9.0/Meta.dll"
-
-open Meta
-open Meta.Solution
-
-let basePath = __SOURCE_DIRECTORY__
-
-basePath |> Stats.FromSrcFile 2020 1
---
+++ Meta/Library.fs
@@ -0,0 +1,76 @@
+﻿namespace Meta
+
+open System.IO
+open System
+
+module SrcHelper =
+    let parseTitle (title: string) =
+        let parseLeft (yearInfo: string) =
+            match yearInfo.Replace("Year", "").TrimStart().Split("/") with
+            | [| year; day |] -> Some((int year), (int day))
+            | _ -> None
+
+        if title.TrimEnd().EndsWith("???") then
+            None
+        else
+            match title.Split("-", StringSplitOptions.TrimEntries) with
+            | [| yearInfo; title |] ->
+                let year, day = (parseLeft yearInfo) |> Option.get
+                Some(year, day, title)
+
+            | _ -> None
+
+    let extractComments year day baseDir =
+        Path.Join(baseDir, $"Year{year}", $"Day%02d{day}.fs")
+        |> File.ReadLines
+        |> Seq.take 5
+        |> List.ofSeq
+
+module Solution =
+    type Difficulty =
+        | XS
+        | S
+        | M
+        | L
+        | XL
+        | NA
+
+        static member FromString =
+            function
+            | "xs" -> XS
+            | "s" -> S
+            | "m" -> M
+            | "l" -> L
+            | "xl" -> XL
+            | _ -> NA
+
+    type Stats =
+        { Year: int
+          Day: int
+          Title: string
+          Difficulty: Difficulty
+          Tags: string[]
+          Remarks: string }
+
+        static member FromSrcFile year day basePath =
+            let tokens =
+                basePath
+                |> SrcHelper.extractComments year day
+                |> List.map (
+                    _.Replace("///", "").TrimStart().Split(":", StringSplitOptions.TrimEntries)
+                    >> Array.last
+                )
+
+            match SrcHelper.parseTitle tokens[0] with
+            | Some(year, day, title) ->
+                match tokens with
+                | [ _; _; difficulty; tags; remarks ] ->
+                    { Year = year
+                      Day = day
+                      Title = title
+                      Difficulty = Difficulty.FromString(difficulty.ToLower())
+                      Tags = tags.Split(" ")
+                      Remarks = remarks }
+                    |> Some
+                | _ -> failwith "Invalid parsing"
+            | _ -> None
