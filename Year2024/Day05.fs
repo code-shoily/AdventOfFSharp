46cac23d2ec286a72d699e6072fae57110815afc
Mafinar Khan <mafinar@gmail.com>
2026-03-16 21:52:15 -0400

    Add Yog. Update 2024/5 to use Yog

--- Year2024/Day05.fs
+++ Year2024/Day05.fs
@@ -7,6 +7,8 @@ module Year2024.Day05
 
 open Common.Helpers
 open Common.Types
+open Yog
+open Yog.Builder
 
 [<AutoOpen>]
 module Helpers =
@@ -17,34 +19,52 @@ module Helpers =
         | [ a; b ] -> (a, b)
         | _ -> unreachable ()
 
-    let updateIsValid (left, right) = left = right
+    let parseRules: (string list -> (int * int) list) =
+        List.map (_.Split("|") >> List.ofArray >> List.map int >> asTuple)
 
-    let buildPrecedenceTable: (string list -> Set<int * int>) =
-        List.map _.Split("|")
-        >> List.map (List.ofArray >> List.map int >> asTuple)
-        >> Set.ofList
+    let parseUpdates: (string list -> int list list) =
+        List.map (_.Split(",") >> List.ofArray >> List.map int)
 
-    let buildPageUpdateOrders: (string list -> int[] list) =
-        List.map (_.Split(",") >> Array.map int)
+    let reorder (rules: (int * int) list) (update: int list) =
+        let updateSet = Set.ofList update
 
+        let builder =
+            rules
+            |> List.filter (fun (a, b) -> updateSet.Contains a && updateSet.Contains b)
+            |> List.fold (fun builder (a, b) -> Labeled.addSimpleEdge a b builder) (Labeled.directed<int, int> ())
+
+        let graph =
+            update |> List.fold (fun b p -> Labeled.addNode p b) builder |> Labeled.toGraph
+
+        match Traversal.topologicalSort graph with
+        | Ok sortedIds ->
+            sortedIds
+            |> List.map (fun id ->
+                match Map.tryFind id graph.Nodes with
+                | Some label -> label
+                | None -> unreachable ())
+        | Error () -> unreachable ()
+
+    let isValid rules update = update = reorder rules update
 
 let parse =
     List.ofSeq
     >> paragraphs
     >> asTuple
-    >> (fun (a, b) -> buildPrecedenceTable a, buildPageUpdateOrders b)
-
-let getCurrentAndDesiredUpdates (table: Set<int * int>) =
-    let sorter (left: int) (right: int) =
-        if table.Contains(left, right) then -1
-        elif table.Contains(right, left) then 1
-        else 0
-
-    List.map (fun update -> (update, update |> Array.sortWith sorter))
-
-let solvePart1 = List.filter updateIsValid >> List.sumBy (fst >> median)
-let solvePart2 = List.filter (updateIsValid >> not) >> List.sumBy (snd >> median)
+    >> (fun (a, b) -> parseRules a, parseUpdates b)
 
 let solve (rawInput: string seq) =
-    let input = rawInput |> parse ||> getCurrentAndDesiredUpdates
-    BothInt(solvePart1 input, solvePart2 input)
+    let rules, updates = parse rawInput
+
+    let part1 =
+        updates
+        |> List.filter (isValid rules)
+        |> List.sumBy (List.toArray >> median)
+
+    let part2 =
+        updates
+        |> List.filter (isValid rules >> not)
+        |> List.map (reorder rules)
+        |> List.sumBy (List.toArray >> median)
+
+    BothInt(part1, part2)
